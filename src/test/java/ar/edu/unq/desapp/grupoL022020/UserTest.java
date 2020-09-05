3a9d3422234989dd17bcc0292b335ce6a5d8f55e
lordmaster11 <jmdicostanzo11@gmail.com>
2020-09-05 18:51:14 -0300

    class creation2

--- src/test/java/ar/edu/unq/desapp/grupoL022020/UserTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoL022020/UserTest.java
@@ -1,18 +1,12 @@
 package ar.edu.unq.desapp.grupoL022020;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.desapp.grupoL022020.model.User;
 
 public class UserTest {
-	User aUser;
-
-	@Before
-	public void setUp() throws Exception {
-		aUser = new User ("Juan", "juan@gmail.com", "1234");
-	}
+	User aUser = new User ("Juan", "juan@gmail.com", "1234");
 
 	@Test
 	public void getNombreTest(){ 	
