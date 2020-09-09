215600c10d49cb7616e29a0a770886a8e61100a9
lordmaster11 <jmdicostanzo11@gmail.com>
2020-09-08 21:15:52 -0300

    user.donate()

--- src/main/java/ar/edu/unq/desapp/grupoL022020/model/Donation.java
+++ src/main/java/ar/edu/unq/desapp/grupoL022020/model/Donation.java
@@ -3,19 +3,31 @@ package ar.edu.unq.desapp.grupoL022020.model;
 import java.util.Calendar;
 
 public class Donation {
-	Calendar dateDonation;
-	Integer amount;
-	User user;
-	
-	public Donation(User myUser, Integer myAmount){
-		dateDonation = Calendar.getInstance();
-		amount = myAmount;
-		user = myUser;
+	private User user;
+	private Project project;
+	private Calendar dateDonation;
+	private Integer amount;
+
+	public Donation(User aUser, Project aProject, Calendar aDate, Integer anAmount) {
+		this.user = aUser;
+		this.project = aProject;
+		this.dateDonation = aDate;
+		this.amount = anAmount;
+	}	
+			
+	public User getUser() {
+		return user;
 	}
-	
+
+	public Project getProject() {
+		return project;
+	}
+
+	public Calendar getDateDonation() {
+		return dateDonation;
+	}
+
 	public Integer getAmount() {
 		return amount;
 	}
-	
-
 }
--- src/test/java/ar/edu/unq/desapp/grupoL022020/UserTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoL022020/UserTest.java
@@ -1,20 +1,75 @@
 package ar.edu.unq.desapp.grupoL022020;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 
+import ar.edu.unq.desapp.grupoL022020.model.Location;
+import ar.edu.unq.desapp.grupoL022020.model.Project;
 import ar.edu.unq.desapp.grupoL022020.model.User;
 
 public class UserTest {
-	User aUser = new User ("Juan", "juan@gmail.com", "1234");
-
 	@Test
-	public void getNombreTest(){ 	
+	public void newUser(){ 
+		User aUser = new User ("Juan", "juan@gmail.com", "1234");
 		assertTrue(aUser.getName().equals("Juan"));	
-	}
-		
-	@Test
-	public void getMail(){ 	
 		assertTrue(aUser.getMail().equals("juan@gmail.com"));
-	}	
+	}
+	
+	@Test
+	public void donateInAProjectWhithLessThan$1000(){ 	
+		User aUser = new User ("Pepe", "pepe@gmail.com", "1234");
+
+		Project project = mock(Project.class);
+		Location location = mock(Location.class);
+		when(project.getLocationProject()).thenReturn(location);
+		when(location.getPopulation()).thenReturn(30000);
+				
+		aUser.donate(500, project);
+		assertTrue(aUser.getPoints().equals(0));
+		assertTrue(aUser.getDonations().size()==(1));		
+	}
+	
+	@Test
+	public void donateInAProjectWhithMoreThan$1000(){ 	
+		User aUser = new User ("Pepe", "pepe@gmail.com", "1234");
+
+		Project project = mock(Project.class);
+		Location location = mock(Location.class);
+		when(project.getLocationProject()).thenReturn(location);
+		when(location.getPopulation()).thenReturn(30000);
+				
+		aUser.donate(1500, project);
+		assertTrue(aUser.getPoints().equals(1500));
+		assertTrue(aUser.getDonations().size()==(1));		
+	}
+	
+	@Test
+	public void donateInAProjectInATownWhithLessThan2000Populations(){ 	
+		User aUser = new User ("Esteban", "esteban@gmail.com", "1234");
+
+		Project project = mock(Project.class);
+		Location location = mock(Location.class);
+		when(project.getLocationProject()).thenReturn(location);
+		when(location.getPopulation()).thenReturn(1000);
+				
+		aUser.donate(500, project);
+		assertTrue(aUser.getPoints().equals(1000));
+		assertTrue(aUser.getDonations().size()==(1));		
+	}
+	
+	@Test
+	public void donateInAProjectInATownWhithLessThan2000PopulationsAndMoreThan$1000(){ 	
+		User aUser = new User ("Coco", "coquito@gmail.com", "1234");
+
+		Project project = mock(Project.class);
+		Location location = mock(Location.class);
+		when(project.getLocationProject()).thenReturn(location);
+		when(location.getPopulation()).thenReturn(1000);
+				
+		aUser.donate(2000, project);
+		assertTrue(aUser.getPoints().equals(6000));
+		assertTrue(aUser.getDonations().size()==(1));		
+	}
 }
