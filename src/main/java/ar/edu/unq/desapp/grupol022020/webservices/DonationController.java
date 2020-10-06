054ab91c61346cd3a2a7c167dbaf955e265f3574
lordmaster11 <jmdicostanzo11@gmail.com>
2020-10-06 03:02:32 -0300

    swagger integration

--- src/main/java/ar/edu/unq/desapp/grupol022020/webservices/DonationController.java
+++ src/main/java/ar/edu/unq/desapp/grupol022020/webservices/DonationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.unq.desapp.grupol022020.model.Donation;
@@ -21,5 +22,11 @@ public class DonationController {
     public ResponseEntity<?> allUsers() {
         List<Donation> list = donationService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+    
+    @GetMapping("/api/donations/{id}")
+    public ResponseEntity<?> getDonation(@PathVariable("id") Integer id) {
+        Donation donation = donationService.findByID(id);
+        return ResponseEntity.ok().body(donation);
     }
 }
--- src/main/java/ar/edu/unq/desapp/grupol022020/webservices/LocationController.java
+++ src/main/java/ar/edu/unq/desapp/grupol022020/webservices/LocationController.java
@@ -1,16 +1,12 @@
 package ar.edu.unq.desapp.grupol022020.webservices;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.unq.desapp.grupol022020.model.Location;
@@ -27,13 +23,10 @@ public class LocationController {
         List<Location> list = locationService.findAll();
         return ResponseEntity.ok().body(list);
     }
-
-    @RequestMapping(value = "/api/version", method = RequestMethod.GET)
-	@ResponseBody
-	public ResponseEntity<?> getVersion() {
-    	String version = "0.2.1";
-		Map<String, String> resultado = new HashMap<String, String>();
-		resultado.put("version", version);
-		return ResponseEntity.ok().body(resultado);
-	}
+    
+    @GetMapping("/api/locations/{id}")
+    public ResponseEntity<?> getLocation(@PathVariable("id") Integer id) {
+        Location location = locationService.findByID(id);
+        return ResponseEntity.ok().body(location);
+    }  
 }
